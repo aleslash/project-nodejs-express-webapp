83425d4726533bd8cc90907e72cae3e2ea99fad3
Alexandre Thomaz <aleslash@gmail.com>
2020-06-09 10:52:52 -0300

    hostname included

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 ﻿'use strict';
 var express = require('express');
 var router = express.Router();
+var os = require("os");
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express', hostname: os.hostname() });
 });
 
 module.exports = router;
